bf9442bb3e50fc26b2e0f8b99ea9939a6df680e7
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-15 09:30:11 +0000

    added postSearch function

--- worldbank/src/Components/Networking.js
+++ worldbank/src/Components/Networking.js
@@ -35,6 +35,25 @@ class Networking {
 
     return json;
   };
+
+  postSearch = async (country, indicator, start, end) => {
+    let response = await fetch(`${url}/createaccount`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        country: country,
+        indicator: indicator,
+        start: start,
+        end: end,
+      }),
+    });
+    const json = await response.json();
+
+    return json;
+  };
 }
 
 export default Networking;
