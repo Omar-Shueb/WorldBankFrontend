ee223cf8d5ac99e944f9ae01405abe5004f94dc5
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-15 16:31:34 +0000

    added persistent login

--- worldbank/src/App.js
+++ worldbank/src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import LogInPage from "./Components/LogInPage/LogInPage";
 
@@ -14,28 +10,62 @@ import SearchPage from "./Components/SearchPage/SearchPage";
 
 import ResultsPage from "./Components/ResultsPage/ResultsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-          <Route path="/results">
-            <ResultsPage />
-          </Route>
-          <Route path="/login">
-            <LogInPage />
-          </Route>
-          <Route path="/register">
-            <RegisterPage />
-          </Route>
-          <Redirect from="/" to="/login" />
-        </Switch>
-      </Router>
-    </div>
-  );
+import Networking from "./Components/Networking";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+  networking = new Networking();
+
+  componentDidMount() {
+    this.checkLogin();
+  }
+
+  checkLogin = async () => {
+    const re = /sessionId=([\w-]+);?/;
+    const cookies = await document.cookie;
+    if (re.test(cookies)) {
+      const loggedIn = await this.networking.getSession();
+      this.setState({ loggedIn: loggedIn });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+    console.log(this.state.loggedIn);
+  };
+
+  loggedIn = () => {
+    return (
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/results">
+          <ResultsPage />
+        </Route>
+        <Redirect from="/" to="/search" />
+      </Switch>
+    );
+  };
+
+  loggedOut = () => {
+    return (
+      <Switch>
+        <Route path="/login">
+          <LogInPage checkLogin={this.checkLogin} />
+        </Route>
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        <Redirect from="/" to="/login" />
+      </Switch>
+    );
+  };
+
+  render() {
+    return <div className="App">{this.state.loggedIn ? this.loggedIn() : this.loggedOut()}</div>;
+  }
 }
 
 export default App;
