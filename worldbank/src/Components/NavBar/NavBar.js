dbb83079252124c9c9a26606ccca71482e7c5dc0
Omar-Shueb <omarshueb@Omars-MacBook-Pro.local>
2022-02-16 17:18:57 +0000

    Stylised NavBar

--- worldbank/src/Components/NavBar/NavBar.js
+++ worldbank/src/Components/NavBar/NavBar.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Button,
+  ButtonGroup,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "./NavBar.css";
 import logo from "./logo.png";
 import Networking from "../Networking";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#ff8a50",
+      main: "#ff5722",
+      dark: "#c41c00",
+      contrastText: "#fafafa",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#f5f5f5",
+      dark: "#c2c2c2",
+      contrastText: "#000",
+    },
+  },
+});
 
 class NavBar extends React.Component {
   networking = new Networking();
@@ -15,30 +40,38 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="header">
-        {/* <img className="logo" alt="world bank logo" src={logo}></img> */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li>
-                <img className="logo" alt="world bank logo" src={logo}></img>
-              </li>
-              <li className="nav-item active">
-                <Link to="/search">
-                  <button className="search-btn">Search</button>
-                </Link>
-              </li>
-
-              <li className="nav-item dropdown">
-                <Link className="history" to="/history">
-                  <button className="history-btn">History</button>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <button onClick={this.handleLogout}>Log out</button>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <ThemeProvider theme={theme}>
+          <AppBar color="primary">
+            <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  mr: 2,
+                  display: { md: "flex" },
+                  background: "#ffffff",
+                  height: "9vh",
+                  width: "9vw",
+                }}
+              >
+                <img className="logo" alt="world bank logo" src={logo} />
+                <ButtonGroup color="secondary" variant="text">
+                  <Button component={Link} to={"/search"}>
+                    Search
+                  </Button>
+                  <Button component={Link} to={"/history"}>
+                    History
+                  </Button>
+                </ButtonGroup>
+              </Typography>
+              <div>
+                <Button color="secondary" onClick={this.handleLogout}>
+                  Log Out
+                </Button>
+              </div>
+            </Toolbar>
+          </AppBar>
+        </ThemeProvider>
       </div>
     );
   }
